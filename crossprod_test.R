41f286a7dda57da7e78f3c4961f50d9483ab00b8
Albert Kuo <albertkuo@uchicago.edu>
2018-07-05 15:10:31 -0400

    Add test of crossprod on list of matrices

--- crossprod_test.R
+++ crossprod_test.R
@@ -1,4 +1,4 @@
-# 6/26
+# 7/5
 # Test crossprod function for HDF5 matrices
 
 # Resources:
@@ -8,7 +8,6 @@ library(BiocParallel)
 library(DelayedArray)
 library(HDF5Array)
 
-read_time = 0
 ITER <- function(x, grid = NULL) {
   grid <- DelayedArray:::.normarg_grid(grid, x)
   b <- 0L
@@ -17,11 +16,9 @@ ITER <- function(x, grid = NULL) {
       return(NULL)
     b <<- b + 1L
     viewport <- grid[[b]]
-    start_time = proc.time()
     block <- DelayedArray:::extract_block(x, viewport)
     if (!is.array(block))
       block <- DelayedArray:::.as_array_or_matrix(block)
-    read_time = read_time + (proc.time() - start_time)
     attr(block, "from_grid") <- grid
     attr(block, "block_id") <- b
     block
@@ -38,21 +35,41 @@ REDUCE <- function(x, y) {
 
 row_n = 100 # Number of rows per block read
 X <- DelayedArray(matrix(rnorm(50000*1000), ncol=1000))
-system.time({
-  X_disk = writeHDF5Array(X, chunkdim = c(row_n, ncol(X)))
-})
-X = DelayedArray(X_disk)
 G <- RegularArrayGrid(dim(X), c(row_n, ncol(X)))
 
+## Test 1
+# Regular crossprod
 system.time({
   ref <- crossprod(as.matrix(X))
 })
 
+## Test 2
+# Crossprod on DelayedArray of in memory matrix
 system.time({
   out <- bpiterate(ITER(X, G), FUN, REDUCE = REDUCE, BPPARAM = MulticoreParam(2))
 })
 
-print(read_time)
+## Test 3
+# Crossprod on DelayedArray of HDF5 matrix
+use_HDF5 = T
+if(use_HDF5){
+  system.time({
+    X_disk = writeHDF5Array(X, chunkdim = c(row_n, ncol(X)))
+  })
+  X2 = DelayedArray(X_disk)
+}
+system.time({
+  out2 <- bpiterate(ITER(X2, G), FUN, REDUCE = REDUCE, BPPARAM = MulticoreParam(2))
+})
+
+## Test 4
+# Crossprod on list of matrices
+blocks_n = 50000/row_n # Length of list
+X_ls  = lapply(1:blocks_n, function(i) matrix(rnorm(row_n*1000), ncol=1000))
+system.time({
+  crossprod_ls = lapply(X_ls, function(X) crossprod(X))
+  out3 = Reduce(REDUCE, crossprod_ls)
+})
 
 all.equal(out, ref)
 
